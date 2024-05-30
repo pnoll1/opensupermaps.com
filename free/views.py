86b567fc327ffc93f144bdc8ef1ed8e484aa40da
pat <pat@debian.lan>
2024-05-30 11:13:58 -0700

    1st try boto

--- free/views.py
+++ free/views.py
@@ -8,6 +8,7 @@ import re
 import json
 from operator import itemgetter
 from pathlib import Path
+import boto3
 
 # timezone class
 class Utc(datetime.tzinfo):
@@ -19,6 +20,9 @@ class Utc(datetime.tzinfo):
         return self.__class__._dst
 
 utc = Utc()
+
+s3 = boto3.client('s3')
+
 def list_map_files(directory):
     '''
     input directory path as string
@@ -26,12 +30,13 @@ def list_map_files(directory):
     '''
     files_obf = []
     files_mwm = []
-    for file in Path(directory).iterdir():
-        modified_time = datetime.datetime.fromtimestamp(file.stat().st_mtime).strftime('%m-%d-%y')
-        size = round(file.stat().st_size / 1048576)
+
+    for file in s3.Bucket('my-bucket').objects.all():
+        modified_time = file.last_modified
+        size = round(file.size / 1048576)
         name = file.name
         if file.suffix == '.obf':
-            sha_file = file.with_suffix('.sha256')
+            sha_file = name.with_suffix('.sha256')
             files_obf.append([file, file.stem.replace('_',' ').upper(), modified_time, size, sha_file, sha_file.stem.replace('_', ' ').upper()])
         #if file.suffix == '.mwm':
         #    files_mwm.append([name, name_short, modified_time, size])
